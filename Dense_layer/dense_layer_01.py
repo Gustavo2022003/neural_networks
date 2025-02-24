557bbc0d5088c8d7035a4d1847224d6281e5e9fc
Gustavo2022003 <playsslinky@gmail.com>
2025-02-24 15:02:31 -0300

    📝docs: changing the comments to english

--- Dense_layer/dense_layer_01.py
+++ Dense_layer/dense_layer_01.py
@@ -1,12 +1,12 @@
 """
-Primeira implementação de redes neurais: camadas densas e propagação direta.
+Dense layers and direct propagation
 """
 
 import numpy as np
 
 np.random.seed(0)
 
-# Dados de entrada X
+# Input data X
 X = [[1, 2, 3, 2.5],
 	[2.0, 5.0, -1.0, 2.0],
 	[-1.5, 2.7, 3.3, -0.8]]
@@ -16,16 +16,16 @@ class Layer_Dense:
 		self.weight = 0.10 * np.random.randn(n_inputs, n_neurons)
 		self.biases = np.zeros((1, n_neurons))
 
-	# Calcula a saída da camada
+	# Calculate the layer output
 	def forward(self, inputs):
 		self.output = np.dot(inputs, self.weight) + self.biases
 
-# Definindo a estrutura da rede
-Layer1 = Layer_Dense(4, 5) # 4 entradas para 5 neurônios
-Layer2 = Layer_Dense(5, 2) # 5 entradas para 2 neurônios
+# Setting up the network structure
+Layer1 = Layer_Dense(4, 5) # 4 inputs for 5 neurons
+Layer2 = Layer_Dense(5, 2) # 5 inputs for 2 neurons
 
 
-Layer1.forward(X) # inicializa a camada 1 com os valores de X
-Layer2.forward(Layer1.output) # inicializa a camada 2 com os valores de saída da camada 1
+Layer1.forward(X) # Initialize the layer 1 with the input data X
+Layer2.forward(Layer1.output) # Initialize the layer 2 with the output data from layer 1
 
-print(Layer2.output) # Imprime no terminal os valores de saída da camada 2
+print(Layer2.output) # Print the output values from the layer 2
