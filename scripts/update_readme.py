8b93acf2214f8f4a0c0e8fc24ad821d3053c81b0
Gustavo2022003 <playsslinky@gmail.com>
2025-02-27 15:27:15 -0300

    :recycle: refactor: change all the update_readme.py algorithm

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -1,34 +1,48 @@
-from jinja2 import Template
 import os
-import datetime
+import re
+from datetime import datetime
 
-def update_readme(file_size, last_update):
-    readme_path = "./README.md"
-    with open(readme_path, "r", encoding="utf-8") as file:
-        template_content = file.read()
+def get_file_size(path):
+    try:
+        file_size = os.path.getsize(path)
+        
+        size_in_mb = file_size / (1024 * 1024)
+        
+        return f"{size_in_mb:.2f} MB"
+    except FileNotFoundError:
+        return "Arquivo não encontrado."
 
-    template = Template(template_content)
+def find_and_replace_pattern_in_md(path, pattern, replacement):
+    try:
+        if pattern == "X.XX MB":
+            regex_pattern = r'(\d+\.\d+ MB)'
+        elif pattern == "MM/DD/YYYY":
+            regex_pattern = r'(\d{2}/\d{2}/\d{4})'
 
-    content = template.render(
-        file_size=file_size,
-        last_update=last_update
-    )
+        with open(path, 'r') as file:
+            content = file.read()
 
-    with open("README.md", "w", encoding="utf-8") as file:
-        file.write(content)
+        updated_content = re.sub(regex_pattern, replacement, content)
 
-def get_file_info():
-    file_path = "./docs/Articles/pt-br/Redes Neurais Artificiais - ptBR.pdf"
-    
-    file_size = os.path.getsize(file_path) / (1024 * 1024)
-    file_size = round(file_size, 2)
+        with open(path, 'w') as file:
+            file.write(updated_content)
+        
+        return f"Padrão '{pattern}' substituído por '{replacement}' no arquivo."
 
-    file_size = f"{file_size} MB"
-    
-    last_modified_time = os.path.getmtime(file_path)
-    last_update = datetime.datetime.fromtimestamp(last_modified_time).strftime('%m/%d/%y')
-    
-    return file_size, last_update
+    except FileNotFoundError:
+        return "Arquivo não encontrado."
+    except Exception as e:
+        return f"Erro ao processar o arquivo: {e}"
 
-file_size, last_update = get_file_info()
-update_readme(file_size, last_update)
+if __name__ == "__main__":
+    current_date = datetime.now()
+    path_to_pdf = './docs/Articles/pt-br/Redes Neurais Artificiais - ptBR.pdf'
+    path_to_readme = "./README.md"
+
+    pattern_to_find = "X.XX MB"
+    replacement_value = str(get_file_size(path_to_pdf))
+
+    pattern_to_find = "MM/DD/YYYY"
+    replacement_value = current_date.strftime("%m/%d/%Y")
+
+    print(find_and_replace_pattern_in_md(path_to_readme, pattern_to_find, replacement_value))
