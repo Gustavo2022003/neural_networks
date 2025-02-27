34e5c577455cccef99fc06acf28796969940ed95
Gustavo2022003 <playsslinky@gmail.com>
2025-02-27 14:33:42 -0300

    :fire: hotfix: formatting adjusted

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -20,8 +20,10 @@ def update_readme(file_size, last_update):
 def get_file_info():
     file_path = "./docs/Articles/pt-br/Redes Neurais Artificiais - ptBR.pdf"
     
-    file_size = f"{os.path.getsize(file_path) / (1024 * 1024)} MB"
+    file_size = os.path.getsize(file_path) / (1024 * 1024)
     file_size = round(file_size, 2)
+
+    file_size = f"{file_size} MB"
     
     last_modified_time = os.path.getmtime(file_path)
     last_update = datetime.datetime.fromtimestamp(last_modified_time).strftime('%m/%d/%y')
